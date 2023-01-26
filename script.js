f4f77513a682b66f63ee7149736e79fffd3961aa
Christian Baez <christian@magentrack.com>
2023-01-26 17:19:49 -0500

    cambios

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function menuOnClick() {
     document.getElementById("menu-bg").classList.toggle("change-bg");
     document.getElementById("burger").style.display = "none";
 }
+
+const checkpoint = 300;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 0.9;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".change-bg").style.opacity = opacity;
+});
